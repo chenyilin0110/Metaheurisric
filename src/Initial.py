cc14fa421e536ff473ee1dace84835b4b68fbc1e
陳羿霖 <a0989735018@gmail.com>
2019-09-30 10:56:07 +0800

    add dimension in Initial Function

--- src/Initial.py
+++ src/Initial.py
@@ -1,15 +1,12 @@
 import numpy as np
 import random
 
-def initial(data, city):
-    solution = np.zeros((city, 3))
+def initial(data, city, dim):
+    dim += 1 #point_number, x, y
+    solution = np.zeros((city, dim))
     for eachcity in range(city):
         r = random.randint(0, len(data)-1)
         for eachdim in range(np.size(data,1)):
             solution[eachcity][eachdim] = data[r][eachdim]
         data = np.delete(data, r, 0)
     return solution
-
-def initial_sa(bit):
-
-    return 0
