41670478f6654b78476c6d127ea5412fec4502d6
陳羿霖 <a0989735018@gmail.com>
2019-09-30 10:56:48 +0800

    add dimension in HillClimbing

--- src/HillClimbing.py
+++ src/HillClimbing.py
@@ -2,29 +2,30 @@ import numpy as np
 import sys
 from Initial import initial
 from Transaction import transaction
-from Determine import determine
 from Evaluation import evaluation
+from Determine import determine
 
 iteration = sys.argv[1]
 city = sys.argv[2]
+dim = sys.argv[3]
 
 best_value = 0
 eachiteration = 0
 
 data = np.loadtxt('dataSet/' + city + '.txt', dtype=np.str, delimiter=" ")
 
-solution = initial(data, int(city))
+solution = initial(data, int(city), int(dim))
+best_solution = solution.copy()
+best_value = evaluation(solution)
 
 while(eachiteration != int(iteration)):
     old_solution,  solution = transaction(solution)
 
-    distance = determine(solution)
-
-    if eachiteration == 0:
-        best_value = distance
-    else:
-        old_distance = determine(old_solution)
-        best_value, solution = evaluation(old_solution, old_distance, solution, distance)
+    distance = evaluation(solution)
+    
+    best_value, best_solution = determine(best_solution, best_value, solution, distance)
+    
+    solution = best_solution.copy()
     
     print(eachiteration + 1, best_value)
     
