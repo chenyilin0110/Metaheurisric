3d40779972062a24c13e91dd28d1bc25262c07c0
陳羿霖 <a0989735018@gmail.com>
2019-10-09 11:23:39 +0800

    add updte neighbor solution function in Update file

--- src/Update.py
+++ src/Update.py
@@ -1,3 +1,7 @@
-def update(rate, temperature):
+def update_neighbor_solution(neighbor_number, best_solution, solution):
+    for each_neighbor_number in range(neighbor_number):
+        solution[each_neighbor_number] = best_solution.copy()
+    return solution
+def update_temperature(rate, temperature):
     temperature = float(rate) * float(temperature)
     return temperature
